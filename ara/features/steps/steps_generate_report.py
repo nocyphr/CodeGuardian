2f7b3ff8c55107a77c6a6297d553469b5021fc37
nocyphr <nocyphr@outlook.com>
2023-12-12 17:19:59 +0200

    update: parametrized stubs

--- ara/features/steps/steps_generate_report.py
+++ ara/features/steps/steps_generate_report.py
@@ -1,5 +1,5 @@
-@given(u'a file in the input folder "./input/code_file.py"')
-def step_impl(context):
+@given(u'a file in the input folder "{input_file_path}"')
+def step_impl(context, input_file_path):
     raise NotImplementedError(u'STEP: Given a file in the input folder "./input/code_file.py"')
 
 
@@ -8,21 +8,11 @@ def step_impl(context):
     raise NotImplementedError(u'STEP: When I analyze the file')
 
 
-@then(u'i find a json file "./output/report.json"')
-def step_impl(context):
+@then(u'i find a json file "{output_file_path}"')
+def step_impl(context, output_file_path):
     raise NotImplementedError(u'STEP: Then i find a json file "./output/report.json"')
 
 
-@then(u'the datapoint path contains ./input/code_file.py')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the datapoint path contains ./input/code_file.py')
-
-
-@then(u'the datapoint avg_cc contains 2.0')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the datapoint avg_cc contains 2.0')
-
-
-@then(u'the datapoint total_cc contains 4.0')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the datapoint total_cc contains 4.0')
+@then(u'the datapoint {datapoint} contains {data}')
+def step_impl(context, datapoint, data):
+    raise NotImplementedError(u'STEP: Then the datapoint {datapoint} contains {data}')
