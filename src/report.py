e80534c163fa5c6436cc8b48832f0d6d3645c1ec
nocyphr <nocyphr@outlook.com>
2023-12-13 09:00:43 +0200

    update: made the test pass

--- src/report.py
+++ src/report.py
@@ -1,7 +1,19 @@
 from os.path import exists
 
-def generate_report(file_path):
+
+
+def read_file(file_path): 
+    with open(file_path) as file: 
+        file_content: str = file.read()
+    return file_content
+
+
+def generate_report(file_path: str):
     if not exists(file_path):
         raise FileNotFoundError(f'no file found at {file_path}')
+
+    
+
+
     with open('./output/report.json', 'w'):
         ...
