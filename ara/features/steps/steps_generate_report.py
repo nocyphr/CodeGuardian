d9a75f423d19c6cdbb924244aaedca86729468a9
nocyphr <nocyphr@outlook.com>
2023-12-13 08:23:12 +0200

    update: adjusted steps + passing given and when for first scenario

--- ara/features/steps/steps_generate_report.py
+++ ara/features/steps/steps_generate_report.py
@@ -1,16 +1,23 @@
-@given(u'a file in the input folder "{input_file_path}"')
+from os.path import exists
+from src.report import generate_report
+
+@given(u'a file "{input_file_path}"')
 def step_impl(context, input_file_path):
-    raise NotImplementedError(u'STEP: Given a file in the input folder "./input/code_file.py"')
+    context.input_file_path = input_file_path
 
 
 @when(u'I analyze the file')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I analyze the file')
+    generate_report(context.input_file_path)
 
 
 @then(u'i find a json file "{output_file_path}"')
 def step_impl(context, output_file_path):
-    raise NotImplementedError(u'STEP: Then i find a json file "./output/report.json"')
+    assert exists(output_file_path), f'file: {output_file_path} was not found'
+
+@when(u'I read the file')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I read the file')
 
 
 @then(u'the datapoint {datapoint} contains {data}')
