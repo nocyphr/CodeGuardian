196750a50ea9c9e6a8aad2475658f47b2360b831
nocyphr <nocyphr@outlook.com>
2023-12-13 08:46:30 +0200

    update: made a failing test for error invalid input path

--- test/test_report.py
+++ test/test_report.py
@@ -1,6 +1,17 @@
 from os.path import exists
+from json import load
 from src.report import generate_report
+from pytest import raises
+
+
 
 def test_that_report_file_is_created():
     generate_report('no/path')
     assert exists('./output/report.json')
+
+
+def test_that_generate_report_fails_for_invalid_input_path():
+    invalid_path = 'invalid/path'
+    with raises as e:
+        generate_report(invalid_path)
+    assert f'no file found at {invalid_path}' in e.value 
