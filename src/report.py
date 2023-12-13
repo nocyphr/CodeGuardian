f4ff8d0958dda85b457cbe5b5699d3cb7c6918c7
nocyphr <nocyphr@outlook.com>
2023-12-13 08:50:50 +0200

    update: implement logic to make new test pass

--- src/report.py
+++ src/report.py
@@ -1,3 +1,7 @@
+from os.path import exists
+
 def generate_report(file_path):
+    if not exists(file_path):
+        raise FileNotFoundError(f'no file found at {file_path}')
     with open('./output/report.json', 'w'):
         ...
