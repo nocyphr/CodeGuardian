dcc4aa7096fb10c7184f91ef043dee91006a58b1
nocyphr <nocyphr@outlook.com>
2023-12-13 09:03:21 +0200

    refactor: move error handling of file open to readfile function, call readfile function in generate report

--- src/report.py
+++ src/report.py
@@ -2,18 +2,17 @@ from os.path import exists
 
 
 
-def read_file(file_path): 
+def read_file(file_path: str) -> str: 
+    if not exists(file_path):
+        raise FileNotFoundError(f'no file found at {file_path}')
+
     with open(file_path) as file: 
         file_content: str = file.read()
     return file_content
 
 
 def generate_report(file_path: str):
-    if not exists(file_path):
-        raise FileNotFoundError(f'no file found at {file_path}')
-
-    
-
+    code_content: str = read_file(file_path)
 
     with open('./output/report.json', 'w'):
         ...
