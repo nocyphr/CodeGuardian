26bf1ffaa9f01e6978bd4cefe485377f55458745
nocyphr <nocyphr@outlook.com>
2023-12-13 13:26:24 +0200

    update: passing unit-test and steps

--- src/report.py
+++ src/report.py
@@ -1,6 +1,8 @@
 from os.path import exists
 from json import dump
 
+from lizard import analyze_file
+
 
 def read_file(file_path: str) -> str: 
     if not exists(file_path):
@@ -16,8 +18,19 @@ def read_file(file_path: str) -> str:
 def add_path_to_report_dict(file_path: str) -> dict: 
     return {'path': file_path}
 
+def calculate_avg_cc(file_path: str) -> float: 
+    analysis_results: object = analyze_file(file_path)
+    avg_cc: float = analysis_results.average_cyclomatic_complexity
+    return avg_cc
+
+    # # List and print the CCN for each function
+    # print("\nCCN per function:")
+    # for function in analysis_results.function_list:
+    #     print(f"Function: {function.name}, CCN: {function.cyclomatic_complexity}")
+
 def generate_report(file_path: str, output_path='./output/report.json'):
     report_dict: dict = add_path_to_report_dict(file_path)
+    report_dict['avg_cc'] = calculate_avg_cc(file_path)
     code_content: str = read_file(file_path)
 
 
