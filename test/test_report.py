57ba4213817bc1fb22937ac9920b005e64865a22
nocyphr <nocyphr@outlook.com>
2023-12-13 08:58:55 +0200

    update: created a failing test for a read_file function that does not exist yet

--- test/test_report.py
+++ test/test_report.py
@@ -1,6 +1,6 @@
 from os.path import exists
 from json import load
-from src.report import generate_report
+from src.report import generate_report, read_file
 from pytest import raises
 
 
@@ -15,3 +15,7 @@ def test_that_generate_report_fails_for_invalid_input_path():
     with raises(FileNotFoundError) as e:
         generate_report(invalid_path)
     assert f'no file found at {invalid_path}' in str(e.value)
+
+def test_that_read_file_returns_non_empty_string():
+    result: str = read_file('./input/code_file.py')
+    assert len(result) > 0
